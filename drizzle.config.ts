afe87b4ab12d0e40a5dd576afb890646839e3dcb
Leonardo Gurgel <leo@gurgel.io>
2024-06-13 21:01:57 -0300

    use prisma

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "drizzle-kit";
-
-export default {
-	schema: "./db/schema.ts",
-	dialect: "sqlite",
-	dbCredentials: {
-		url: "file:./db/db.sqlite",
-	},
-} satisfies Config;
